ac36a89cd04b0d9bca37906e34ecd3ff6334a3d3
Fernando <a21110132@ceti.mx>
2023-11-06 14:58:12 -0600

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.java
@@ -0,0 +1,67 @@
+package com.example.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+
+import com.example.chatapp.R;
+import com.example.chatapp.adapters.ChatAdapter;
+import com.example.chatapp.databinding.ActivityChatBinding;
+import com.example.chatapp.models.ChatMessage;
+import com.example.chatapp.models.User;
+import com.example.chatapp.utilities.Constants;
+import com.example.chatapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private ActivityChatBinding binding;
+    private User receiverUser;
+
+    private List<ChatMessage> chatMessages;
+    private ChatAdapter chatAdapter;
+    private PreferenceManager preferenceManager;
+    private FirebaseFirestore database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+        loadingReceiverDetails();
+        init();
+    }
+
+    private void init(){
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        chatMessages = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                chatMessages,
+                preferenceManager.getString(Constants.KEY_USER_ID),
+                getBitmaoFromEncodedeString(receiverUser.image)
+                );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        database = FirebaseFirestore.getInstance();
+    }
+
+    private Bitmap getBitmaoFromEncodedeString(String encodedImage){
+        byte [] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
+    }
+
+    private void loadingReceiverDetails(){
+        receiverUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
+        binding.textName.setText(receiverUser.name);
+    }
+
+    private void setListeners(){
+        binding.imageBack.setOnClickListener(view -> onBackPressed());
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/lesteners/UserListener.java
@@ -0,0 +1,8 @@
+package com.example.chatapp.lesteners;
+
+import com.example.chatapp.models.User;
+
+public interface UserListener {
+
+    void userClicked(User user);
+}
